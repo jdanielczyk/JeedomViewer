4733084489a0681bf23b9ae7b82d5ac99c8d5045
julien <julien.danielczyk@gmail.com>
2021-04-18 10:01:11 +0200

    change app tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,29 +1,39 @@
-import { act, render, screen } from '@testing-library/react';
-import {ProvideAuth} from './use-auth'
+import { act, render, screen, fireEvent } from '@testing-library/react';
+import { ProvideAuth } from './use-auth'
 import App from './App';
-
+import { flushSync } from 'react-dom';
 
 beforeEach(() => fetch.resetMocks());
 
-
 test('user is not authent return to login', async () => 
 {
-    // mockUser = null;
-    //return to login
-    fetch.mockResponseOnce({success:false});
+    fetch.mockResponse({success:false});
 
     await act(async () => render(
         <ProvideAuth>
             <App />
         </ProvideAuth>));
+
     expect(screen.getAllByText(/Login/g)).toHaveLength(2);
 });
 
 
-// test('user is authent', () => 
-// {
-//     //display data
-//     throw new Error();
-// });
+test('user is authent', async () => 
+{
+    fetch.mockResponse(JSON.stringify({success:true}));
+
+    await act(async () => render(
+        <ProvideAuth>
+            <App />
+        </ProvideAuth>));
+
+
+    fireEvent.click(screen.getByRole('button',/Login/i));
+
+    await flushSync();
+
+    expect(fetch).toHaveBeenCalledTimes(2);
+    expect(screen.getAllByAltText(/Logout/g)).toHaveLength(1);
+});
 
 
